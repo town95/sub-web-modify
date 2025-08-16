b62a0d1b85e5ad2d681e1cc8ddf3d1ff51b93774
town95 <town95@qq.com>
2025-08-16 10:26:55 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -338,7 +338,7 @@ export default {
           "suo.yt": "https://suo.yt/short",
         },
         customBackend: {
-           "自用专属后端": "https://subapi.tangshuai.xx.kg", 
+           "自用专属后端": "https://subapi.tangshuai.dpdns.org", 
           "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
           "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
@@ -1311,4 +1311,5 @@ export default {
   }
 };
 </script>
+
 
